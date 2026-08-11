7470cea0c0e782984cfce4ef840e96473b06710e
jingji <hieu0901179787@gmail.com>
2026-08-11 18:11:27 +0700

    a

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -3,6 +3,6 @@ import { Platform } from 'react-native';
 // Android Emulators resolve host localhost via 10.0.2.2.
 // iOS Simulators and Web resolve host localhost via 127.0.0.1 / localhost.
 export const API_BASE_URL = Platform.select({
-  android: 'http://192.168.88.198:5212',
-  default: 'http://192.168.88.198:5212',
+  android: 'http://10.10.89.95:5212',
+  default: 'http://10.10.89.95:5212',
 });
